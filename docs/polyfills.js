51beadd56527f915934d268b3b1dea8b1655de88
eliseunetto <eliseunetto@hotmail.com>
2021-05-12 21:42:23 -0300

    Update readme

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\temp\ws-angular\TESTE\bookstore\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\temp\ws-angular\bookstore-frontend\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
